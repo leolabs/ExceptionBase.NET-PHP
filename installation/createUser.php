df7c0b0519f1ebe0e98d52846e8611f33e18dc63
Leo Bernard <admin@leolabs.org>
2012-12-31 00:09:02 +0100

    SQL-Befehle verbessert
    Überprüfung, ob Benutzer schon Besteht, vor dem Eintragen

--- installation/createUser.php
+++ installation/createUser.php
@@ -1,5 +1,5 @@
 <?php
-	include("incs/database.php");
+	include("../incs/database.php");
 	
 	$user = mysql_real_escape_string($_GET["user"]);
 	$pass = mysql_real_escape_string($_GET["pass"]);
@@ -8,6 +8,13 @@
 	
 	$request = utf8_decode("INSERT INTO users (Username, PasswordHash, Mail) VALUES ('$user', '$passhash', '$mail')");
 	
-	$result = mysql_query($request) or die ("Request failed: " . mysql_error());
+	$doubleCheck = mysql_query("SELECT * FROM users WHERE Username='$user'");
+	
+	if(mysql_num_rows($doubleCheck) == 0){
+		$result = mysql_query($request) or die ("Request failed: " . mysql_error());
+	} else {
+		$result = "Der Benutzer $user existiert bereits in der Datenbank.";
+	}
+	
 	echo $result;
 ?>
